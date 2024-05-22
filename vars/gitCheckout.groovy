8be791a92348e81a5e7aaa555f09e8702d5fb550
fatimabutt1899 <66645119+fatimabutt1899@users.noreply.github.com>
2024-05-22 16:09:53 +0500

    Update gitCheckout.groovy
    git credentials reader added

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,6 +3,8 @@ def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
+        userRemoteConfigs: [[ url: stageParams.url,
+        credentialsId: stageParams.credentialsId
+       ]]
     ])
   }
